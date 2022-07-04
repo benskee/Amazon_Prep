ec328a8a46d7a19ebfb0dbe163172668fc9fc546
benskee <benskee33@gmail.com>
2022-07-04 09:26:31 -0400

    improved euler 14

--- euler/14.js
+++ euler/14.js
@@ -1,28 +1,69 @@
 // Longest Collatz sequence
 
-const collatz = num => {
-    let seqs = {1: 0};
-    let max = [1, 0];
-
-    const coll = n => {
-        let y = 1;
-        if(n % 2 == 0) {
-            y = n / 2;
-        } else {
-           y = 3 * n + 1
-        }
-        return y in seqs ? seqs[y] + 1 : coll(y) + 1
+const collatz = () => {
+    let chains = {};
+let chain = [];
+let max = 999999;
+let previousChainLength = 0;
+let longestChainLength = 0;
+let longestChainValue = 0;
+for (i = 1; i <= max; i++) {
+  let v = i;
+  while (v > 1) {
+    if (chains[v] != undefined) {
+      previousChainLength = chains[v] - 1;
+      break;
     }
-
-    for(let i=2; i<num; i += 10000) {
-        for(let j=0; j<10000; j++) {
-            if(i+j < num) seqs[i + j] = coll(i + j)
-        }
-        let newMax = Math.max(...Object.values(seqs))
-       if(newMax > max[1]) max = [Object.keys(seqs).filter(key => seqs[key] == newMax)[0], newMax] 
-       seqs = {1:0}
+    if (v % 2 == 0) {
+      v /= 2;
     }
-    return max[0]
+    else {
+      v = 3 * v + 1;
+    }
+    chain.push(v);
+  }
+  while (chain.length > 0) {
+    if (chain.length + previousChainLength > longestChainLength) {
+      longestChainLength = chain.length + previousChainLength;
+      longestChainValue = i;
+    }
+    chains[chain[0]] = chain.length + previousChainLength;
+    chain.shift();
+  }
+}
+return longestChainValue
+
+
+
+
+
+
+
+
+
+
+    // let seqs = {1: 0};
+    // let max = [1, 0];
+
+    // const coll = n => {
+    //     let y = 1;
+    //     if(n % 2 == 0) {
+    //         y = n / 2;
+    //     } else {
+    //        y = 3 * n + 1
+    //     }
+    //     return y in seqs ? seqs[y] + 1 : coll(y) + 1
+    // }
+
+    // for(let i=2; i<num; i += 10000) {
+    //     for(let j=0; j<10000; j++) {
+    //         if(i+j < num) seqs[i + j] = coll(i + j)
+    //     }
+    //     let newMax = Math.max(...Object.values(seqs))
+    //    if(newMax > max[1]) max = [Object.keys(seqs).filter(key => seqs[key] == newMax)[0], newMax] 
+    //    seqs = {1:0}
+    // }
+    // return max[0]
 }
 
-console.log(collatz(1000000))
+console.log(collatz())

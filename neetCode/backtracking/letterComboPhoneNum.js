0196a5fa7d5fde09850427542fd1f71de160b4f2
benskee <benskee33@gmail.com>
2022-07-27 13:51:52 -0400

    letterComboPhoneNum

---
+++ neetCode/backtracking/letterComboPhoneNum.js
@@ -0,0 +1,25 @@
+// Letter Combination of a Phone Number - Medium
+
+const letterCombinations = digits => {
+    if(!digits.length) return []
+    const letters = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+    let ans = []
+
+    const combos = (sub, nums) => {
+        console.log(sub,nums)
+        if(!nums.length) {
+            ans.push(sub)
+            return
+        }
+        let currentLetters = letters[parseInt(nums[0])]
+        nums = nums.slice(1)
+        for(let i=0; i<currentLetters.length; i++) {
+            combos(sub+currentLetters[i], nums)
+        }
+    }
+    combos('', digits)
+    return ans
+}
+
+let d1 = '23'
+console.log(letterCombinations(d1))
